d1af207abb7df01abb4235c2e88ad0d508aa2323
Astra Jessamy <astrajessamy@macbook-pro.mynetworksettings.com>
2025-09-05 10:23:55 -0400

    move app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import"bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
-  const [movies, setMovies] = useState([        {
-            "Title": "Star Wars: Episode IV - A New Hope",
-            "Year": "1977",
-            "imdbID": "tt0076759",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode V - The Empire Strikes Back",
-            "Year": "1980",
-            "imdbID": "tt0080684",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VI - Return of the Jedi",
-            "Year": "1983",
-            "imdbID": "tt0086190",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNWEwOTI0MmUtMGNmNy00ODViLTlkZDQtZTg1YmQ3MDgyNTUzXkEyXkFqcGc@._V1_SX300.jpg"
-        },
-      ]);
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+      const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=25ec181`
+        const response = await fetch(url);
+        const responseJson = await response.json();
+        if (responseJson.Search) {
+        setMovies(responseJson.Search);
+        }
+      };
+
+      useEffect(() => {
+        getMovieRequest(searchValue);
+      }, [searchValue]);
+
+      useEffect(() => {
+        const movieFavourites = JSON.parse(
+          localStorage.getItem('react-movie-app-favourites')
+        );
+
+        setFavourites(movieFavourites);
+      }, []);
+
+      const saveToLocalStorage = (items) => {
+        localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+      };
+
+      const addFavouriteMovie = (movie) => {
+        const newFavouriteList = [...favourites, movie];
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+      };
+
+      const removeFavouriteMovie = (movie) => {
+        const newFavouriteList = favourites.filter(
+          (favourite) => favourite.imdbID !== movie.imdbID
+        );
+
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+      };
+
   return   (
-    <div className>
-    <MovieList movies={movies} />
-  </div>
+    <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      /</div>
+      <div className='row'>
+          <MovieList 
+              movies={movies} 
+              handleFavouritesClick={addFavouriteMovie} 
+              favouriteComponent ={AddFavourites}/>
+       </div>
+       <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Favourites'/>
+      </div>
+            <div className='row'>
+          <MovieList 
+              movies={favourites} 
+              handleFavouritesClick={addFavouriteMovie} 
+              favouriteComponent ={RemoveFavourites}
+          />
+       </div>
+       </div>
   );
 };
 
